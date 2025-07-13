f8b62a02d3b4837892400a45a71b87c901facf8b
JJBcku <154745800+JJBcku@users.noreply.github.com>
2025-07-13 22:25:23 +0200

    Recompiled shaders

--- JJs2DEngine/StandardShaders/compileShaders.bat
+++ JJs2DEngine/StandardShaders/compileShaders.bat
@@ -1,11 +1,11 @@
-%VULKAN_SDK%\Bin\glslc.exe UILayerShader.vert -O -o UILayerShader.vert.spv
-%VULKAN_SDK%\Bin\glslc.exe StandardFragmentShader.frag -O -o StandardFragmentShader.frag.spv
-%VULKAN_SDK%\Bin\glslc.exe UILayerShader.vert -O0 -o UILayerShader.debug.vert.spv
-%VULKAN_SDK%\Bin\glslc.exe StandardFragmentShader.frag -O0 -o StandardFragmentShader.debug.frag.spv
+%VULKAN_SDK%\Bin\glslc.exe UILayerShader.vert --target-env=vulkan -O -o UILayerShader.vert.spv
+%VULKAN_SDK%\Bin\glslc.exe StandardFragmentShader.frag --target-env=vulkan -O -o StandardFragmentShader.frag.spv
+%VULKAN_SDK%\Bin\glslc.exe UILayerShader.vert --target-env=vulkan -O0 -o UILayerShader.debug.vert.spv
+%VULKAN_SDK%\Bin\glslc.exe StandardFragmentShader.frag --target-env=vulkan -O0 -o StandardFragmentShader.debug.frag.spv
 
-%VULKAN_SDK%\Bin\glslc.exe UILayerShader.vert -S -O -o UILayerShader.vert.txt
-%VULKAN_SDK%\Bin\glslc.exe StandardFragmentShader.frag -S -O -o StandardFragmentShader.frag.txt
-%VULKAN_SDK%\Bin\glslc.exe UILayerShader.vert -S -O0 -o UILayerShader.debug.vert.txt
-%VULKAN_SDK%\Bin\glslc.exe StandardFragmentShader.frag -S -O0 -o StandardFragmentShader.debug.frag.txt
+%VULKAN_SDK%\Bin\glslc.exe UILayerShader.vert --target-env=vulkan -S -O -o UILayerShader.vert.txt
+%VULKAN_SDK%\Bin\glslc.exe StandardFragmentShader.frag --target-env=vulkan -S -O -o StandardFragmentShader.frag.txt
+%VULKAN_SDK%\Bin\glslc.exe UILayerShader.vert --target-env=vulkan -S -O0 -o UILayerShader.debug.vert.txt
+%VULKAN_SDK%\Bin\glslc.exe StandardFragmentShader.frag --target-env=vulkan -S -O0 -o StandardFragmentShader.debug.frag.txt
 
 pause
