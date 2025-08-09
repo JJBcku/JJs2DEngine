fdc5d4d6e4f9c56eaf7d1914bf6ee1e5d5790409
JJBcku <154745800+JJBcku@users.noreply.github.com>
2025-08-09 15:26:12 +0200

    Added 8 and 4k textures compiler option

--- JJs2DEngine/Include/Common/TextureArraySize.h
+++ JJs2DEngine/Include/Common/TextureArraySize.h
@@ -2,7 +2,15 @@
 
 #include <stdint.h>
 
+#if defined(JJ2DENGINE_8K_TEXTURES_ENABLED)
 constexpr uint32_t skippedSizeLevels = 6;
+#elseif defined(JJ2DENGINE_4K_TEXTURES_ENABLED)
+constexpr uint32_t skippedSizeLevels = 5;
+#else
+constexpr uint32_t skippedSizeLevels = 4;
+#endif
+
+
 constexpr uint32_t imagesInTextureArray = 8;
 constexpr uint32_t textureArraysAmount = 2;
 constexpr uint32_t imagesInAllTextureArrays = imagesInTextureArray * textureArraysAmount;
