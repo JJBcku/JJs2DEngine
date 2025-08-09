f4898e057e4e41e680ea66a7155d59932ca5d232
JJBcku <154745800+JJBcku@users.noreply.github.com>
2025-08-09 21:32:43 +0200

    Added no texture size shift compiler settings

--- JJs2DEngine/Include/Common/TextureArraySize.h
+++ JJs2DEngine/Include/Common/TextureArraySize.h
@@ -10,7 +10,13 @@ constexpr uint32_t skippedSizeLevels = 5;
 constexpr uint32_t skippedSizeLevels = 4;
 #endif
 
-
+#if defined(JJ2DENGINE_8K_TEXTURES_ENABLED_NO_SIZE_SHIFT)
+constexpr uint32_t imagesInTextureArray = 10;
+#elseif defined (JJ2DENGINE_4K_TEXTURES_ENABLED_NO_SIZE_SHIFT)
+constexpr uint32_t imagesInTextureArray = 9;
+#else
 constexpr uint32_t imagesInTextureArray = 8;
+#endif
+
 constexpr uint32_t textureArraysAmount = 2;
 constexpr uint32_t imagesInAllTextureArrays = imagesInTextureArray * textureArraysAmount;
