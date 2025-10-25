239e8b773eec5419eccd7841ad70d79ee7fcb778
JJBcku <154745800+JJBcku@users.noreply.github.com>
2025-10-25 02:52:13 +0200

    Changed initial camera position

--- JJs2DEngineTest/CreateLayers.cpp
+++ JJs2DEngineTest/CreateLayers.cpp
@@ -36,7 +36,7 @@ void CreateLayers(MainDataCollection& data)
 
 	data.objectID = optionalObjectID.value();
 
-	vertexDataMain.SetCameraPosition(0.0f, 0.0f);
+	vertexDataMain.SetCameraPosition(2.0f, 0.0f);
 	vertexDataMain.SetCameraZoom(0.5f);
 
 }
